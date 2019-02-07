bd7b4a35b283c605c282d480445aaa4f7e55bb28
Sveg <36763190+Sveg@users.noreply.github.com>
2019-02-07 12:59:28 +0100

    Så er der startet

---
+++ ConsoleAppDatabase/Facade.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleAppDatabase
+{
+    public static class Facade
+    {
+        public static List<Student> GetAllStudent()
+        {
+
+        }
+    }
+}
---
+++ ConsoleAppDatabase/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ConsoleAppDatabase
+{
+    public class Student
+    {
+        public Student(int studentid, string name, string mobilnr)
+        {
+            studentid = Studentid;
+            name = Name;
+            mobilnr = Mobilnr;
+        }
+
+        public int Studentid { get; set; }
+
+        public string Name { get; set; }
+
+        public string Mobilnr { get; set; }
+
+        
+    }
+}
